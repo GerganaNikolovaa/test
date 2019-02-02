c7dadb2dcaea5ad9c1122269e85a7f8743b862cb
GerganaNikolovaa <geri_pn@abv.bg>
2019-02-02 16:55:09 +0200

    Updating

---
+++ 08.11.2018.2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+bool isin(int n,int k)
+{
+    if(n==0)return false;
+    if(n%10==k)return true;
+    else return isin(n/10,k);
+}
+int msin()
+{
+    int n,k;
+    cin>>k>>n;
+    int a[n];
+    for(int i=0;i<n;i++)cin>>a[i];
+    for(int i=0;i<n;i++)
+    {
+        if()
+    }
+}
